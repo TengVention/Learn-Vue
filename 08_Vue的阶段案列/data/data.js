b5f0d038515a8261e1564d9e4f63bd53d8a5c645
tengvention <tengvention@163.com>
2023-05-16 23:10:50 +0800

    Vue阶段案例

---
+++ 08_Vue的阶段案列/data/data.js
@@ -0,0 +1,30 @@
+const books = [
+    {
+        id: 1,
+        name: "《算法导论》",
+        date: "2006-09",
+        price: 85.00,
+        count: 1
+    },
+    {
+        id: 2,
+        name: "《数据结构》",
+        date: "2008-10",
+        price: 100.00,
+        count: 1
+    },
+    {
+        id: 3,
+        name: "《代码大全》",
+        date: "2010-05",
+        price: 123.00,
+        count: 1
+    },
+    {
+        id: 4,
+        name: "《nodeJS从入门到精通》",
+        date: "2015-07",
+        price: 150.00,
+        count: 1
+    }
+]
