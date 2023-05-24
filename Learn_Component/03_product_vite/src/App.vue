d1fcf13c2c102e9f6b6cf4b98cec77941b6b1025
tengvention <564948258@qq.com>
2023-05-24 15:41:24 +0800

    初始化04_learn_component

---
+++ Learn_Component/03_product_vite/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2 class="title">
+      My Application
+    </h2>
+    <AppHeader></AppHeader>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style scoped></style>
