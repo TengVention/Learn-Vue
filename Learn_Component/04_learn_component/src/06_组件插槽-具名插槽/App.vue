356f80dbbabe1313e9ec4705dda52d1b6daff193
tengvention <564948258@qq.com>
2023-05-31 16:52:44 +0800

    具名插槽

---
+++ Learn_Component/04_learn_component/src/06_组件插槽-具名插槽/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="app">
+        <nav-bar>
+            <template v-slot:left>
+                <button>返回</button>
+            </template>
+            <template v-slot:center>
+                <span>内容</span>
+            </template>
+            <template v-slot:right>
+                <a href="#">登录</a>
+            </template>
+        </nav-bar>
+    </div>
+</template>
+
+<script>
+import NavBar from './NavBar.vue';
+export default {
+    components: {
+        NavBar
+    }
+}
+</script>
+
+<style scoped></style>
--- Learn_Component/04_learn_component/src/main.js
+++ Learn_Component/04_learn_component/src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 
 // import App from './01_组件的嵌套使用/App.vue'
 // import App from './02_组件的通信-父传子/App.vue'
-import App from './04_组件通信-案例练习/App.vue'
+import App from './06_组件插槽-具名插槽/App.vue'
 
 createApp(App).mount('#app')
