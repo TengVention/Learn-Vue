9fd257422c6559054555e0858491c1f570c90fe5
tengvention <564948258@qq.com>
2023-07-06 09:38:22 +0800

    修改

---
+++ Learn_Component/04_learn_component/src/08_Provide和Inject/HomeBanner.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="banner">
+        <h2>HomeBanner: aaa</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
--- Learn_Component/04_learn_component/src/main.js
+++ Learn_Component/04_learn_component/src/main.js
@@ -3,6 +3,6 @@ import { createApp } from 'vue'
 // import App from './01_组件的嵌套使用/App.vue'
 // import App from './02_组件的通信-父传子/App.vue'
 // import App from './06_组件插槽-具名插槽/App.vue'
-import App from './07_组件通信-作用域插槽/App.vue'
+import App from './08_Provide和Inject/App.vue'
 
 createApp(App).mount('#app')
